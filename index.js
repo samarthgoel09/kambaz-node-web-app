6455e9c30261ba7cf7cdc077297b046d801d9d32
Samarth goel <goel.sama@northeastern.edu>
2025-06-18 00:38:34 +0530

    assignment6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
-
+import mongoose from "mongoose";
 import Hello from './Hello.js';
 import Lab5 from './Lab5/index.js';
 import UserRoutes from './Kambaz/Users/routes.js';
@@ -10,7 +10,8 @@ import CourseRoutes from './Kambaz/Courses/routes.js';
 import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
-
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 const allowedOrigins = [
